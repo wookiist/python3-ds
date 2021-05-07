cda167e81815d2079310e9c03523b4815c1a17da
Jaewook Oh <kyle.oh95@gmail.com>
2021-05-07 22:56:47 +0900

    2021-05-07 boj 1912

---
+++ 1912.py
@@ -0,0 +1,10 @@
+import sys; input = sys.stdin.readline
+n = int(input())
+A = [0] + [int(x) for x in input().split()]
+D = [0 for _ in range(n+1)]
+for i in range(1, n+1):
+    if D[i-1] + A[i] > A[i]:
+        D[i] = D[i-1] + A[i]
+    else:
+        D[i] = A[i]
+print(max(D[1:]))
